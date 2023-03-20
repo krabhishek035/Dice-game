6f09d8a12d311e5509f7a0b28e739827319ffd7e
krabhishek035 <113412604+krabhishek035@users.noreply.github.com>
2023-03-20 22:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // DICE1
 var RandomNumber1 = Math.floor(Math.random()*6)+1;
 var RandomDiceImage1 = "dice" + RandomNumber1 + ".png";
-var RandomImageSource1 = "images/" + RandomDiceImage1;
+var RandomImageSource1 =  RandomDiceImage1;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",RandomImageSource1);
 
@@ -10,6 +10,6 @@ image1.setAttribute("src",RandomImageSource1);
 // DICE2
 var RandomNumber2 = Math.floor(Math.random()*6)+1 ;
 var RandomDiceImage2 = "dice" + RandomNumber2 + ".png";
-var RandomImageSource2 = "images/" + RandomDiceImage2;
+var RandomImageSource2 =  RandomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",RandomImageSource2);
