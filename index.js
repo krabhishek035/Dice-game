d04ffdbde99f6837e0ad90ff52ec6b2a3e30ea79
krabhishek035 <113412604+krabhishek035@users.noreply.github.com>
2023-03-20 22:46:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// DICE1
+var RandomNumber1 = Math.floor(Math.random()*6)+1;
+var RandomDiceImage1 = "dice" + RandomNumber1 + ".png";
+var RandomImageSource1 = "images/" + RandomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",RandomImageSource1);
+
+
+// DICE2
+var RandomNumber2 = Math.floor(Math.random()*6)+1 ;
+var RandomDiceImage2 = "dice" + RandomNumber2 + ".png";
+var RandomImageSource2 = "images/" + RandomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",RandomImageSource2);
